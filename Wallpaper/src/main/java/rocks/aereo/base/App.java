f3937cf1ab3adc748371d2a499069ebb7f9c8b87
robineco <31357538+robineco@users.noreply.github.com>
2019-12-30 15:57:39 +0100

    check every 10 min

--- Wallpaper/src/main/java/rocks/aereo/base/App.java
+++ Wallpaper/src/main/java/rocks/aereo/base/App.java
@@ -5,10 +5,8 @@ import java.util.Timer;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
         Timer timer = new Timer();
         DailyTask dailyTask = new DailyTask(args[0]);
-
-        timer.scheduleAtFixedRate(dailyTask, 0 , 86400000);
+        timer.scheduleAtFixedRate(dailyTask, 0 , 3600);
     }
 }
--- Wallpaper/src/main/java/rocks/aereo/base/reader/JSONReader.java
+++ Wallpaper/src/main/java/rocks/aereo/base/reader/JSONReader.java
@@ -6,7 +6,6 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class JSONReader {
